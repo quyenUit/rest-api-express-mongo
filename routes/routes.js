a9c202d1ba8e661230856c1e6fda532a3c35c65c
quyenUit <nguyenquyenboss@gmail.com>
2022-09-25 15:38:18 +0700

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const Model = require("../models/models");
+const routes = express.Router();
+
+//get all api
+routes.get("/getAll", async (req, res) => {
+  try {
+    const data = await Model.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+//get by id api
+routes.get("/getOne/:id", async (req, res) => {
+  try {
+    const data = await Model.findById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+//632f52add0cff8d4c55e7402
+
+//post api
+routes.post("/post", async (req, res) => {
+  const data = new Model({
+    name: req.body.name,
+    age: req.body.age,
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//update api
+routes.patch("/update/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
+
+    const result = await Model.findByIdAndUpdate(id, updatedData, options);
+
+    res.send(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//delete api
+routes.delete("/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Model.findByIdAndDelete(id);
+    res.send(`Document with ${data.name} has been deleted..`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = routes;
